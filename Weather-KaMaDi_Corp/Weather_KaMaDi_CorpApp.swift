48e3ab2ebfacb661fbb590a4d66030aa224ae7f0
KaMaDi <artkamadi@gmail.com>
2024-05-31 09:03:55 +0300

    Initial Commit

---
+++ Weather-KaMaDi_Corp/Weather_KaMaDi_CorpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_KaMaDi_CorpApp.swift
+//  Weather-KaMaDi_Corp
+//
+//  Created by Jedi on 31.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Weather_KaMaDi_CorpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
